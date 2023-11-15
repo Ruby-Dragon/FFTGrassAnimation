e8a8e1011270a15f06a076dae3673750b9ce0a7f
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2023-11-15 11:28:50 -0800

    add compute shader base

--- GrassField.cs
+++ GrassField.cs
@@ -3,9 +3,16 @@ using System;
 
 public partial class GrassField : MultiMeshInstance3D
 {
+	private RenderingDevice ComputeRunner;
+	private Rid ComputeShader;
+    
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		ComputeRunner = RenderingServer.CreateLocalRenderingDevice();
+		var ShaderFile = GD.Load<RDShaderFile>("res://WindStrengthComputeShader.glsl");
+		var ShaderByteCode = ShaderFile.GetSpirV();
+		ComputeShader = ComputeRunner.ShaderCreateFromSpirV(ShaderByteCode);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
